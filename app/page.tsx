5b83d7d100f85400b4977c0fa814fe1c0307a0da
Sunday Alabi <iamalabisunday@gmail.com>
2025-09-29 23:31:46 -0700

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center text-4xl uppercase text-[var(--orange-100)] font-bold">
+      I love JavaScript
+    </div>
+  );
+}
