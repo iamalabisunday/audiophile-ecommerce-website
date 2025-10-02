79895d5c9e752466e571c48a24e1d71216b87b62
Sunday Alabi <iamalabisunday@gmail.com>
2025-10-01 19:40:37 -0700

    initial commit

--- app/navigationbar/navbar.tsx
+++ app/navigationbar/navbar.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { MdOutlineShoppingCart } from "react-icons/md";
 import { TiThMenu } from "react-icons/ti";
+import { usePathname } from "next/navigation";
 
 export default function Navigationbar() {
   const pathname = usePathname();
