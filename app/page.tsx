a74c3a8a2cf88ebac8be2115d4576ebe5fad897d
Sunday Alabi <iamalabisunday@gmail.com>
2025-10-05 03:25:16 -0700

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,29 @@
 export default function Home() {
   return (
     <div className="w-full min-h-screen">
-      <div className="w-full h-full md:min-h-[700px] min-h-[600px] bg-black-200 mx-auto">
-        <div className="max-w-[70rem] mx-auto flex justify-center items-center">
-          <div className="text-white-100 flex flex-col gap-6 items-start">
-            <p className="subheader uppercase opacity-[50%]">New Prouct</p>
-            <span className="headerOne uppercase">XX99 Mark II Headphones</span>
+      <div className="w-full h-full md:min-h-[700px] lg:min-h-[600px] bg-black-200 mx-auto">
+        <div className="max-w-[70rem] mx-auto flex justify-center items-center sm:text-center">
+          <div className="text-white-100 flex flex-col gap-6 items-start absolute lg:static z-inset text-center lg:text-start md:max-w-[380px] max-w-[328px] lg:max-w-full justify-center">
+            <p className="w-full lg:w-fit text-center subheader uppercase opacity-[50%]">
+              New Prouct
+            </p>
+            <span className="headerThree md:headerOne uppercase md:bg-red-900 bg-blue-900">
+              XX99 Mark II Headphones
+            </span>
             <p className="bodytext max-w-[360px] opacity-[75%]">
               Experience natural, lifelike audio and exceptional build quality
               made for the passionate music enthusiast.
             </p>
-            <button className="subTitle uppercase bg-orange-100 px-6 py-3 cursor-pointer">
-              See Product
-            </button>
+            <div className="lg:w-fit w-full">
+              <button className="subTitle uppercase bg-orange-100 px-6 py-3 cursor-pointer">
+                See Product
+              </button>
+            </div>
           </div>
           <img
             src="./HomeImages/Bitmap.png"
             alt="Bitmap image"
-            className="w-fit"
+            className="md:w-fit w-full"
           />
         </div>
       </div>
